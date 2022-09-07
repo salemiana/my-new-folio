9846fdaa1bf5e388de6e62c7638b00914bec51b0
salem z <salem.zerezghi2@gmail.com>
2022-09-06 20:31:28 -0600

    created about component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
